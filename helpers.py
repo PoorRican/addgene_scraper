916b694f933d9ec2ee4ef11647e81ef9f7b818cc
Josue Figueroa <poor.rican@pm.me>
2023-08-06 16:02:47 -0700

    Helper function to get inner text

--- helpers.py
+++ helpers.py
@@ -1,2 +1,17 @@
+from bs4 import Tag, NavigableString
+
+
 def build_url(endpoint: str) -> str:
-    return f'https://www.addgene.org/{endpoint}'
+    return f'https://www.addgene.org/{endpoint}/'
+
+
+# NOTE: these are helper functions to scrape primitive data
+
+# TODO: add test for primitive data
+def get_inner_string(tag: Tag) -> str:
+    """ Get text enclosed by a tag, ignoring any other contained text.
+    """
+    for s in tag.children:
+        if type(s) == NavigableString:
+            return str(s).strip()
+
