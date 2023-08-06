4ef24217f73e3f34de5263c8e2c5de73744e0a35
Josue Figueroa <poor.rican@pm.me>
2023-08-06 16:03:59 -0700

    Update failing tests for scraping plasmid page

--- tests/plasmid.py
+++ tests/plasmid.py
@@ -1,20 +1,18 @@
 import unittest
-from requests import get
 
-from helpers import build_url
+from scrapers.plasmid import PlasmidScraper
 
 # TODO: "Tag/Fusion Protein" does not parse correctly
 EXPECTED = {
     '128041': {
         'Vector backbone': {'value': 'pBABE puro', 'href': '/vector-database/query/?q_vdb=pBABE%20puro'},
         'Vector type': {'value': 'Mammalian Expression, Retroviral', 'href': None},
-        '/ Fusion Protein': {'value': [], 'href': None},
         'Bacterial Resistance(s)': {'value': 'Ampicillin, 100 μg/mL', 'href': None},
         'Growth Temperature': {'value': '37°C', 'href': None},
         'Growth Strain(s)': {'value': 'NEB Stable', 'href': None},
         'Copy number': {'value': 'High Copy', 'href': None},
         'Gene/Insert name': {'value': 'None', 'href': None},
-        'Tag/ Fusion Protein': {'value': [], 'href': None},
+        'Tag/ Fusion Protein': {'value': ['EGFP (N terminal on backbone)'], 'href': None},
         'Purpose': {'value': '(Empty Backbone)Retroviral vector with N terminal EGFP', 'href': None},
         'Depositing Lab': {'value': 'Oskar Laur', 'href': '/browse/pi/4513/'},
         'Publication': {'value': 'Emory Custom Cloning Core Plasmids - Oskar Laur (unpublished)',
@@ -26,10 +24,17 @@ EXPECTED = {
 
 class PlasmidPageTests(unittest.TestCase):
     def test_parse_page(self):
-        for key, expected in EXPECTED:
-            response = get(build_url(key))
-            parsed = get_plasmid(response)
-            self.assertEqual(expected, parsed)  # add assertion here
+        # TODO: the tests for checking keys (both ways), and checking values should be split into multiple tests.
+        #  However, GET requests should be executed on the class level ONCE and not per test.
+        for plasmid_id, expected in EXPECTED.items():
+            scraper = PlasmidScraper(plasmid_id)
+            parsed = scraper.scrape()
+            for key, value in expected.items():
+                self.assertTrue(parsed[key], "Scraped values did not contain {key}")
+                self.assertEqual(parsed[key], expected[key], f"Values don't match for {key}")  # add assertion here
+            # also fail if key is not expected (meaning incomplete implementation)
+            for key in parsed.keys():
+                self.assertTrue(key in expected.keys())
 
 
 if __name__ == '__main__':
