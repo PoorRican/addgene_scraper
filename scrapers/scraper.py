5911179480b4aaa4e538dfaebaaaabf2193921af
Josue Figueroa <poor.rican@pm.me>
2023-08-06 14:56:13 -0700

    Clean html before parsing

---
+++ scrapers/scraper.py
@@ -0,0 +1,24 @@
+# base class for implementing parser
+
+from bs4 import BeautifulSoup
+from requests import get
+from abc import ABC
+
+
+def clean_all_newlines(raw: str) -> str:
+    """ Deletes any newlines and cleans up excess whitespace """
+    split = raw.splitlines(False)
+    stripped = [i.strip() for i in split]
+    return ''.join(stripped)
+
+
+class BaseScraper(ABC):
+    def __init__(self, url: str):
+        super().__init__()
+
+        # TODO: constructors shouldn't call blocking function
+        response = get(url)
+        cleaned = clean_all_newlines(response.text)
+        self.soup = BeautifulSoup(cleaned)
+
+    # NOTE: there should be base scrapers for lists, and `a` tags to be reused in specific implementations
