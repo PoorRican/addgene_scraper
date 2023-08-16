66dc9076629abbf850a1e82fa60440b69756d682
Josue Figueroa <poor.rican@pm.me>
2023-08-15 22:20:42 -0700

    Unittests should request single `GET`

--- tests/sequence.py
+++ tests/sequence.py
@@ -8,35 +8,34 @@ EXAMPLES = (
 
 
 class SequenceScraperTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.scrapers = [(i, SequenceScraper(i)) for i in EXAMPLES]
+
     def test_default_url(self):
-        for endpoint in EXAMPLES:
-            scraper = SequenceScraper(endpoint)
-            self.assertEqual(scraper.url, build_url(endpoint))
+        for link, scraper in self.scrapers:
+            self.assertEqual(scraper.url, build_url(link))
 
     def test_has_snapgene(self):
-        endpoint = EXAMPLES[0]
-        scraper = SequenceScraper(endpoint)
+        _, scraper = self.scrapers[0]
         self.assertTrue(scraper.has_snapgene)  # add assertion here
 
     def test_has_genbank(self):
-        endpoint = EXAMPLES[0]
-        scraper = SequenceScraper(endpoint)
+        _, scraper = self.scrapers[0]
         self.assertTrue(scraper.has_genbank)  # add assertion here
 
     def test_is_sequence_page(self):
-        endpoint = EXAMPLES[0]
-        scraper = SequenceScraper(endpoint)
+        _, scraper = self.scrapers[0]
         self.assertTrue(scraper._is_sequence_page())  # add assertion here
 
     def test_get_snapgene(self):
-        endpoint = EXAMPLES[0]
-        scraper = SequenceScraper(endpoint)
+        _, scraper = self.scrapers[0]
         with open('files/128041.snap') as f:
             self.assertEqual(f.read(), scraper.get_snapgene())
 
     def test_get_genbank(self):
-        endpoint = EXAMPLES[0]
-        scraper = SequenceScraper(endpoint)
+        _, scraper = self.scrapers[0]
         with open('files/128041.gbk') as f:
             self.assertEqual(f.read(), scraper.get_genbank())
 
